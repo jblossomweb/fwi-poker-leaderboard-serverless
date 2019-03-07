c7f64e8d42dee51f49aa64d017c4279347faeed1
John Blossom <jblossom.web@gmail.com>
2019-03-07 03:51:46 -0700

    additions to schema

--- middleware.js
+++ middleware.js
@@ -4,6 +4,7 @@ const { cors } = require('middy/middlewares')
 module.exports.applyMiddleware = controller => middlewares(controller)
   .use(cors({
     origins: [
-      'http://localhost:3000',
+      // 'http://localhost:3000', // dev
+      'https://fwi-poker-leaderboard.firebaseapp.com', // prod
     ]
   }))
--- models.js
+++ models.js
@@ -4,6 +4,8 @@ const PlayerSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'required']},
   country: { type: String, required: [true, 'required']},
   winnings: { type: Number, required: [true, 'required']},
+  email: { type: String },
+  twitterHandle: { type: String },
 })
 
 module.exports = {
